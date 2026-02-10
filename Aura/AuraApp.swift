3c5c3a1e97f2a98b73a9cba2c1bdebcb8429b50a
zhge <geo1076229095@gmail.com>
2026-02-10 18:30:05 +0800

    Initial Commit

---
+++ Aura/AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraApp.swift
+//  Aura
+//
+//  Created by jiazhen yan on 2026/2/10.
+//
+
+import SwiftUI
+
+@main
+struct AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aura/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aura
+//
+//  Created by jiazhen yan on 2026/2/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
